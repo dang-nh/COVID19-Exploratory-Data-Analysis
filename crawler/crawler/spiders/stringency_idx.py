0c6bd86b362206aac6dae5671b2c7fe175a35b65
Hoang Dang <dang.nh194423@sis.hust.edu.vn>
2021-12-13 22:27:21 +0700

    Add code via upload

--- crawler/crawler/spiders/stringency_idx.py
+++ crawler/crawler/spiders/stringency_idx.py
@@ -3,14 +3,14 @@ import scrapy
 
 class StringencyIdxSpider(scrapy.Spider):
     name = 'stringency_idx'
-    allowed_domains = ['https://ourworldindata.org/grapher/covid-stringency-index?tab=table']
-    start_urls = ['https://ourworldindata.org/grapher/covid-stringency-index?tab=table/']
+    allowed_domains = ['www.ourworldindata.org']
+    start_urls = ['https://www.ourworldindata.org/grapher/covid-stringency-index?tab=table/']
 
     def parse(self, response):
-        countries = response.xpath("/tr")
+        countries = response.xpath("//tr")
 
         for country in countries:
-            name = country.xpath("(.//td)[1]/text()").get()
+            name = country.xpath("td[@class='entity sorted']/text()").get()
             stringency_idx = country.xpath("(./td)[3]/text()").get()
             abs_change = country.xpath("(./td)[4]/text()").get()
             relative_change = country.xpath("(./td)[5]/text()").get()
